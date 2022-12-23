c2460918e1713958c365b4a66c8c685ba3c557e2
yuliiaantonova <ju4iaJj1985>
2022-12-23 18:50:09 +0100

    validation writing data to files

--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 from util import get_spark_session
 from process import transform
 from read import from_files
+from write import to_files
 import os
 
 
@@ -9,13 +10,13 @@ def main():
     src_dir = os.environ.get('SRC_DIR')
     src_file_pattern = f'{os.environ.get("SRC_FILE_PATTERN")}-*'
     src_file_format = os.environ.get("SRC_FILE_FORMAT")
+    tgt_dir = os.environ.get('TGT_DIR')
+    tgt_file_format = os.environ.get("TGT_FILE_FORMAT")
+
     spark = get_spark_session(env, 'GitHub Activity - Getting Started')
     df = from_files(spark, src_dir, src_file_pattern, src_file_format)
     df_transformed = transform(df)
-    df.printSchema()
-    df_transformed. \
-        select('repo.*', 'created_at', 'year', 'month', 'day'). \
-        show()
+    to_files(df_transformed, tgt_dir, tgt_file_format)
 
 
 if __name__ == '__main__':
